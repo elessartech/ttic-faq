b3f1fd44a85ba5527e3250fba2e7380ae405ce2d
Maksim <ilmastm9@gmail.com>
2018-07-27 17:43:40 +0300

    new

--- app/Router.php
+++ app/Router.php
@@ -19,7 +19,7 @@ class Router {
         elseif (count($url) > 1)
         {
             $route['index'] = $url[0];
-            $route['controller'] = $url[1];
+            $route['controller'] = Router::DEFAULT_OPTION;
         }
 
         $controllerName = ucfirst($route['controller']);
