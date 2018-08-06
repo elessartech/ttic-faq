db93415821a670f26c05262d3c1973f27baa1f3a
Maksim <ilmastm9@gmail.com>
2018-08-06 20:07:24 +0300

    menu improved

--- views/about.php
+++ views/about.php
@@ -19,23 +19,23 @@
 <div class="topnav" id="myTopnav">
 {% if session_admin %}
 	  	<div class="topnav_container">
-			<a href="/">Home</a>
-			<a href="?/about">About</a>
-			<a href="?/login">Log In</a>
-			<a href="#contact">Contact</a>
-			<a href="javascript:void(0);" class="icon" onclick="makeResponsive()">
-				<i class="fa fa-bars"></i>
-			</a>
+      <a href="?/panel">Admin Panel</a>
+		<a href="?/about">About</a>
+		<a href="#contact">Contact</a>
+		<a href="?/login/logout">Log Out</a>
+		<a href="javascript:void(0);" class="icon" onclick="makeResponsive()">
+			<i class="fa fa-bars"></i>
+		</a>
 		</div>
 		{% elseif session_user %}
 		<div class="topnav_container">
-			<a href="/">Home</a>
-			<a href="?/about">About</a>
-			<a href="?/login">Log In</a>
-			<a href="#contact">Contact</a>
-			<a href="javascript:void(0);" class="icon" onclick="makeResponsive()">
-				<i class="fa fa-bars"></i>
-			</a>
+		<a href="?/panel"></i>User Panel</a>
+		<a href="?/about">About</a>
+		<a href="#contact">Contact</a>
+		<a href="?/login/logout">Log Out</a>
+		<a href="javascript:void(0);" class="icon" onclick="makeResponsive()">
+			<i class="fa fa-bars"></i>
+		</a>
 		</div>
 		{% else %}
 		<div class="topnav_container">
